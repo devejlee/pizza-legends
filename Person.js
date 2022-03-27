0b9ad26d9c13dd4823af93eeba63a20894859dc4
Elijah Lee <develijahlee@gmail.com>
2022-03-27 15:24:36 +0900

    add character animations

--- Person.js
+++ Person.js
@@ -15,6 +15,7 @@ class Person extends GameObject {
 
   update(state) {
     this.updatePosition()
+    this.updateSprite(state)
     if (this.isPlayerControlled && this.movingProgressRemaining === 0 && state.arrow) {
       this.direction = state.arrow
       this.movingProgressRemaining = 16
@@ -28,4 +29,15 @@ class Person extends GameObject {
       this.movingProgressRemaining -= 1
     }
   }
+
+  updateSprite(state) {
+    if (this.isPlayerControlled && this.movingProgressRemaining === 0 && state.arrow) {
+      this.sprite.setAnimation('idle-' + this.direction)
+    }
+
+    if (this.movingProgressRemaining > 0) {
+      this.sprite.setAnimation('walk-' + this.direction)
+    }
+  }
+
 }
