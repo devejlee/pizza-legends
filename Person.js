c7c6ba29bd52740c80fdd075e3ebc595b10c29fd
Elijah Lee <develijahlee@gmail.com>
2022-03-30 22:09:07 +0900

    update commas and quotation marks

--- Person.js
+++ Person.js
@@ -1,62 +1,62 @@
 class Person extends GameObject {
   constructor(config) {
-    super(config);
-    this.movingProgressRemaining = 0;
+    super(config)
+    this.movingProgressRemaining = 0
 
-    this.isPlayerControlled = config.isPlayerControlled || false;
+    this.isPlayerControlled = config.isPlayerControlled || false
 
     this.directionUpdate = {
-      "up": ["y", -1],
-      "down": ["y", 1],
-      "left": ["x", -1],
-      "right": ["x", 1],
+      'up': ['y', -1],
+      'down': ['y', 1],
+      'left': ['x', -1],
+      'right': ['x', 1],
     }
   }
 
   update(state) {
     if (this.movingProgressRemaining > 0) {
-      this.updatePosition();
+      this.updatePosition()
     } else {
 
       //More cases for starting to walk will come here
       //
       //
 
-      //Case: We're keyboard ready and have an arrow pressed
+      //Case: We're keyboard ready (it's ok for a player to provide input) and have an arrow pressed
       if (!state.map.isCutscenePlaying && !state.map.isCutscenePlaying && this.isPlayerControlled && state.arrow) {
         this.startBehavior(state, {
-          type: "walk",
+          type: 'walk',
           direction: state.arrow
         })
       }
-      this.updateSprite(state);
+      this.updateSprite(state)
     }
   }
 
   startBehavior(state, behavior) {
     //Set character direction to whatever behavior has
-    this.direction = behavior.direction;
+    this.direction = behavior.direction
 
-    if (behavior.type === "walk") {
+    if (behavior.type === 'walk') {
       //Stop here if space is not free
       if (state.map.isSpaceTaken(this.x, this.y, this.direction)) {
 
         behavior.retry && setTimeout(() => {
           this.startBehavior(state, behavior)
-        }, 10);
+        }, 10)
 
-        return;
+        return
       }
 
       //Ready to walk!
-      state.map.moveWall(this.x, this.y, this.direction);
-      this.movingProgressRemaining = 16;
-      this.updateSprite(state);
+      state.map.moveWall(this.x, this.y, this.direction)
+      this.movingProgressRemaining = 16
+      this.updateSprite(state)
     }
 
-    if (behavior.type === "stand") {
+    if (behavior.type === 'stand') {
       setTimeout(() => {
-        utils.emitEvent("PersonStandComplete", {
+        utils.emitEvent('PersonStandComplete', {
           whoId: this.id
         })
       }, behavior.time)
@@ -65,13 +65,13 @@ class Person extends GameObject {
   }
 
   updatePosition() {
-    const [property, change] = this.directionUpdate[this.direction];
-    this[property] += change;
-    this.movingProgressRemaining -= 1;
+    const [property, change] = this.directionUpdate[this.direction]
+    this[property] += change
+    this.movingProgressRemaining -= 1
 
     if (this.movingProgressRemaining === 0) {
       //We finished the walk!
-      utils.emitEvent("PersonWalkingComplete", {
+      utils.emitEvent('PersonWalkingComplete', {
         whoId: this.id
       })
 
@@ -80,10 +80,10 @@ class Person extends GameObject {
 
   updateSprite() {
     if (this.movingProgressRemaining > 0) {
-      this.sprite.setAnimation("walk-" + this.direction);
-      return;
+      this.sprite.setAnimation('walk-' + this.direction)
+      return
     }
-    this.sprite.setAnimation("idle-" + this.direction);
+    this.sprite.setAnimation('idle-' + this.direction)
   }
 
 }
