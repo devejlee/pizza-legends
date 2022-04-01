8383c42919772a7b07a041e3464b1e423371a0ea
Elijah Lee <develijahlee@gmail.com>
2022-04-01 17:46:09 +0900

    add text message cutscene on page load

--- Overworld.js
+++ Overworld.js
@@ -54,9 +54,10 @@ class Overworld {
     this.map.startCutscene([
       { who: 'hero', type: 'walk', direction: 'down' },
       { who: 'hero', type: 'walk', direction: 'down' },
+      { who: 'npcA', type: 'walk', direction: 'up' },
       { who: 'npcA', type: 'walk', direction: 'left' },
-      { who: 'npcA', type: 'walk', direction: 'left' },
-      { who: 'npcA', type: 'stand', direction: 'up', time: 800 },
+      { who: 'hero', type: 'stand', direction: 'right', time: 200 },
+      { type: 'textMessage', text: 'Why hello there' },
     ])
   }
 }
