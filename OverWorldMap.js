065502dac252d1d5bc180188c617e989ed5d5e52
Elijah Lee <develijahlee@gmail.com>
2022-04-22 23:37:20 +0900

    test overworldmap img paths

--- OverWorldMap.js
+++ OverWorldMap.js
@@ -102,8 +102,8 @@ class OverworldMap {
 window.OverworldMaps = {
   DemoRoom: {
     id: "DemoRoom",
-    lowerSrc: "images/maps/DemoLower.png",
-    upperSrc: "images/maps/DemoUpper.png",
+    lowerSrc: "/images/maps/DemoLower.png",
+    upperSrc: "/images/maps/DemoUpper.png",
     gameObjects: {
       hero: new Person({
         isPlayerControlled: true,
@@ -113,7 +113,7 @@ window.OverworldMaps = {
       npcA: new Person({
         x: utils.withGrid(7),
         y: utils.withGrid(9),
-        src: "images/characters/people/npc1.png",
+        src: "/images/characters/people/npc1.png",
         behaviorLoop: [
           { type: "stand", direction: "left", time: 800 },
           { type: "stand", direction: "up", time: 800 },
@@ -142,7 +142,7 @@ window.OverworldMaps = {
       npcB: new Person({
         x: utils.withGrid(8),
         y: utils.withGrid(5),
-        src: "images/characters/people/erio.png",
+        src: "/images/characters/people/erio.png",
         talking: [
           {
             events: [
@@ -204,8 +204,8 @@ window.OverworldMaps = {
   },
   Kitchen: {
     id: "Kitchen",
-    lowerSrc: "images/maps/KitchenLower.png",
-    upperSrc: "images/maps/KitchenUpper.png",
+    lowerSrc: "/images/maps/KitchenLower.png",
+    upperSrc: "/images/maps/KitchenUpper.png",
     gameObjects: {
       hero: new Person({
         isPlayerControlled: true,
@@ -215,7 +215,7 @@ window.OverworldMaps = {
       npcB: new Person({
         x: utils.withGrid(10),
         y: utils.withGrid(8),
-        src: "images/characters/people/npc3.png",
+        src: "/images/characters/people/npc3.png",
         talking: [
           {
             events: [
@@ -243,8 +243,8 @@ window.OverworldMaps = {
   },
   Street: {
     id: "Street",
-    lowerSrc: "images/maps/StreetLower.png",
-    upperSrc: "images/maps/StreetUpper.png",
+    lowerSrc: "/images/maps/StreetLower.png",
+    upperSrc: "/images/maps/StreetUpper.png",
     gameObjects: {
       hero: new Person({
         isPlayerControlled: true,
